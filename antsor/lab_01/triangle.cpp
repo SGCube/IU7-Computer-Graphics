2ec7b1ea5342d600179fb1c72af5f532b0db45f3
sap17u769 <sortech-sorgamer@yandex.ru>
2019-02-17 21:55:36 +0300

    #2 triangle search function

--- antsor/lab_01/triangle.cpp
+++ antsor/lab_01/triangle.cpp
@@ -37,6 +37,35 @@ QVector2D Triangle::getMinHeight(int *hvertex)
     return QVector2D(QPointF(xm - xp, ym - yp));
 }
 
+QVector2D getMinHeight(float *p1, float *p2, float *p3, int *hvertex)
+{
+	QPointF points[3];
+	points[0].setX(p1[0]);
+	points[0].setY(p1[1]);
+	points[1].setX(p2[0]);
+	points[1].setY(p2[1]);
+	points[2].setX(p3[0]);
+	points[2].setY(p3[1]);
+	
+	float x, y, xm, ym, xp, yp;
+    float h = -1, hm = -1;
+    for (int i = 0; i < 3; i++)
+    {
+        x = points[i].x() + points[(i + 2) % 3].y() - points[(i + 1) % 3].y();
+        y = points[i].y() - points[(i + 2) % 3].x() + points[(i + 1) % 3].x();
+        h = sqrt(x * x + y * y);
+        if (hm == -1 || h < hm)
+        {
+            hm = h;
+            xm = x;
+            ym = y;
+            *hvertex = i;
+            xp = points[i].x();
+            yp = points[i].y();
+        }
+    }
+    return QVector2D(QPointF(xm - xp, ym - yp));
+}
 
 bool isTriangle(float x1, float y1, float x2, float y2, float x3, float y3)
 {
@@ -54,10 +83,29 @@ Triangle solve(float **plist, int n)
 	if (!plist || n < 3)
 		return NULL;
 	
+	float hmin = -1, h = -1;
+	int i1 = 0, i2 = 1, i3 = 2;
+	
 	for (int i = 0; i < n - 2; i++)
 		for (int j = i + 1; j < n - 2; j++)
 			for (int k = j + 1; k < n - 2; k++)
-			{
-				
-			}
+				if (isTriangle(plist[i1][0], plist[i1][1],
+							   plist[i2][0], plist[i2][1],
+							   plist[i3][0], plist[i3][1]))
+				{
+					h = getMinHeight(plist[i], plist[j], plist[k]);
+					if (hmin == -1 || h < hmin)
+					{
+						i1 = i;
+						i2 = j;
+						i3 = k;
+						hmin = h;
+					}
+				}
+	
+	if (hmin == -1)
+		return NULL;
+	return Triangle(plist[i1][0], plist[i1][1],
+			plist[i2][0], plist[i2][1],
+			plist[i3][0], plist[i3][1]);
 }
--- antsor/lab_01/triangle.h
+++ antsor/lab_01/triangle.h
@@ -9,12 +9,14 @@ class Triangle
 public:
     Triangle(float x1, float y1, float x2, float y2, float x3, float y3);
     Triangle(QPointF p1, QPointF p2, QPointF p3);
+	QPointF points[3];
 private:
-    QPointF points[3];
-    QVector2D getMinHeight(int *hvertex);
+	Triangle getMinHeight(int *hvertex);
 };
 
 bool isTriangle(float x1, float y1, float x2, float y2, float x3, float y3);
+
+QVector2D getMinHeight(float *p1, float *p2, float *p3, int *hvertex);
 
 Triangle solve(float **plist, int n);
 
