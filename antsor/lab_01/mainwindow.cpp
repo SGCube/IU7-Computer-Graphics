a01fdc28ef5a11f3d124bf9c0200371f0013e772
sap17u769 <sortech-sorgamer@yandex.ru>
2019-02-17 17:51:15 +0300

    #6 draw button function made

--- antsor/lab_01/mainwindow.cpp
+++ antsor/lab_01/mainwindow.cpp
@@ -31,5 +31,68 @@ void MainWindow::on_delButton_released()
 
 void MainWindow::on_drawButton_released()
 {
+    /// подготовка данных
+    ///
+    int rows = ui->pointTable->rowCount();
+    /// проверка на достаточность
+    if (rows < 3)
+    {
+        ui->msgFrame->setText(QString(
+                                  "Недостаточно данных для решения задачи!"));
+        return;
+    }
 
+    /// выделение памяти под точки
+    float **plist = new float * [rows];
+    if (!plist)
+    {
+        ui->msgFrame->setText(QString(
+                                  "Ошибка выделения памяти!"));
+        return;
+    }
+    for(int i = 0; i < rows; i++)
+    {
+        plist[i] = new float [2];
+        if (!plist[i])
+        {
+            ui->msgFrame->setText(QString(
+                                      "Ошибка выделения памяти!"));
+            for(int j = 0; j < i; j++)
+                delete [] plist[j];
+            delete [] plist;
+            return;
+        }
+    }
+
+    /// проверка на корректность
+    bool correct = true;
+    QString xstr, ystr;
+    int i = 0;
+    bool coord = 0;
+    for(i = 0; i < rows && correct; i++)
+    {
+        xstr = ui->pointTable->item(i, 0)->text();
+        ystr = ui->pointTable->item(i, 1)->text();
+        coord = 0;
+        plist[i][0] = xstr.toFloat(&correct);
+        if (correct)
+        {
+            plist[i][1] = ystr.toFloat(&correct);
+            coord = 1;
+        }
+    }
+    if (!correct)
+    {
+        QString msg("Некорректные данные: точка №");
+        msg.append(QString::number(i));
+        msg.append(", координата ");
+        if (coord == 0)
+            msg.append("X!");
+        else
+            msg.append("Y!");
+        ui->msgFrame->setText(QString(msg));
+    }
+    for(int i = 0; i < rows && correct; i++)
+        delete [] plist[i];
+    delete [] plist;
 }
