c6771b42e50701e1f98125d32cadde9b8645a6c6
pka17u738 <KsenyaPolyakovaaa@mail.ru>
2019-03-03 19:47:30 +0300

    #1 add simple graphics

--- ksupol/lab_02/mainwindow.cpp
+++ ksupol/lab_02/mainwindow.cpp
@@ -5,9 +5,9 @@
 #include <qmath.h>
 #define a 20
 #define b 20
-#define r 2
-#define c 2
-#define d 2
+#define r 30
+#define c 10
+#define d 20
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
@@ -21,18 +21,35 @@ MainWindow::MainWindow(QWidget *parent) :
 
     QPen blueBrush(Qt::blue);
     QPen blackpen(Qt::black);
-    blackpen.setWidth(3);
-    blueBrush.setWidth(3);
+    blackpen.setWidth(1);
+    blueBrush.setWidth(1);
 
     float y = 0;
 
-    for (float x = 0; x < 50; x += 0.1)
+    for (float x = -r; x < r; x += 0.05)
     {
-        y = sqrt(2500 - qPow(x, 2));
-        scene->addEllipse(x, -y, 5, 5, blackpen);
-        scene->addEllipse(x, y, 5, 5, blackpen);
+        y = sqrt(qPow(r, 2) - qPow(x, 2));
+        scene->addEllipse(x, -y, 3, 3, blackpen);
+        scene->addEllipse(x, y, 3, 3, blackpen);
     }
-
+    for (float x = - c; x < 100; x += 0.1)
+    {
+        y = sqrt(x + c);
+        scene->addEllipse(x, -3*y, 3, 3, blackpen);
+        scene->addEllipse(x, 3*y, 3, 3, blackpen);
+    }
+    for (float x = - c; x < r; x += 0.1)
+    {
+        y = sqrt(x + c);
+        for (float ac = 0; ac < y; ac += 0.1)
+        {
+            if (qPow(x, 2) + qPow(ac, 2) < qPow(r, 2))
+            {
+                scene->addEllipse(x, -3*ac, 3, 3, blackpen);
+                scene->addEllipse(x, 3*ac, 3, 3, blackpen);
+            }
+        }
+    }
 }
 
 MainWindow::~MainWindow()
