594e3698dafc6fbda72b075e7843ef11c6c2feb6
SGCube <sortech-sorgamer@yandex.ru>
2019-03-12 17:50:47 +0300

    #29 undo action add

--- antsor/lab_02/window.cpp
+++ antsor/lab_02/window.cpp
@@ -14,6 +14,8 @@ window::window(QWidget *parent) :
 	scene->setSceneRect(0, 0, GVIEW_W, GVIEW_H);
 	ui->gView->setScene(scene);
 	
+	ui->undoButton->setDisabled(true);
+	
 	house_obj.draw(scene);
 }
 
@@ -47,6 +49,7 @@ void window::on_moveButton_released()
 	command_t com = {};
 	set_move(&com, dx, dy);
 	comlist.prepend(com);
+	ui->undoButton->setDisabled(false);
 	
 	if (dx != 0 || dy != 0)
 	{
@@ -96,6 +99,7 @@ void window::on_scaleButton_released()
 	command_t com = {};
 	set_scale(&com, kx, ky, Point(xm, ym));
 	comlist.prepend(com);
+	ui->undoButton->setDisabled(false);
 	
 	if (kx != 1 || ky != 1)
 	{
@@ -138,10 +142,32 @@ void window::on_rotateButton_released()
 	command_t com = {};
 	set_rotate(&com, deg, Point(xc, yc));
 	comlist.prepend(com);
+	ui->undoButton->setDisabled(false);
 	
 	if (deg != 0)
 	{
 		house_obj.rotate(deg, Point(xc, yc));
 		house_obj.draw(scene);
+	}
+}
+
+void window::on_undoButton_released()
+{
+    if (!comlist.isEmpty())
+	{
+		command_t com = comlist.takeFirst();
+		
+		if (com.type == MOVE_COM)
+			house_obj.move(com.args.movarg.dx, com.args.movarg.dy);
+		else if (com.type == SCALE_COM)
+			house_obj.scale(com.args.sclarg.kx, com.args.sclarg.ky,
+							com.args.sclarg.pm);
+		else if (com.type == ROTATE_COM)
+			house_obj.rotate(com.args.rotarg.deg, com.args.rotarg.pc);
+		
+		house_obj.draw(scene);
+		
+		if (comlist.isEmpty())
+			ui->undoButton->setDisabled(true);
 	}
 }
